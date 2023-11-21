ac409feff6fd0d5a6d557073dec610d8af58184f
Mongiellox <122557128+GianfrancoMongiell0@users.noreply.github.com>
2023-11-21 12:56:03 -0400

    csv

--- Main/src/EDD/ArchivoCSV.java
+++ Main/src/EDD/ArchivoCSV.java
@@ -4,10 +4,42 @@
  */
 package EDD;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.nio.Buffer;
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author gianf
  */
 public class ArchivoCSV {
-    
+
+    private BufferedReader lector;
+    private String linea;
+    private String partes[];
+
+    public void leerArchivoCSV(String nombreArchivo) {
+        try {
+            lector = new BufferedReader(new FileReader(nombreArchivo));
+            while ((linea = lector.readLine()) != null) {
+                partes = linea.split(",");
+                imprimirLinea();
+                System.out.println();
+            }
+            lector.close();
+            linea = null;
+            partes = null;
+        } catch (Exception e) {
+            JOptionPane.showConfirmDialog(null, e);
+        }
+
+    }
+
+    public void imprimirLinea() {
+        for (int i = 0; i < partes.length; i++) {
+            System.out.println(partes[i] + "|");
+        }
+    }
+
 }
